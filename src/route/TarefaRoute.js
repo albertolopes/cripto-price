62c299473cf66810ce8be3bab605e4f1fd12a4ab
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-03-11 21:11:52 -0300

    Atualizar

--- src/route/TarefaRoute.js
+++ src/route/TarefaRoute.js
@@ -5,27 +5,25 @@ const router = express.Router();
 const scheduleService = new ScheduleService();
 
 router.get("/noticias", async (req, res) => {
-    // try {
+    try {
         await scheduleService.enviarNoticia();
         res
             .status(200)
             .json({ message: "Tarefa de noticias executada com sucesso!" });
-    // } catch (error) {
-    //     console.error("Erro ao executar a tarefa de preço:", error);
-    //     res.status(500).send("Erro interno do servidor");
-    // }
+    } catch (error) {
+        res.status(500).send("Erro ao executar a tarefa de noticias:" + error);
+    }
 });
 
 router.get("/preco", async (req, res) => {
-    // try {
+    try {
         await scheduleService.enviarPreco();
         res
             .status(200)
             .json({ message: "Tarefa preço executada com sucesso!" });
-    // } catch (error) {
-    //     console.error("Erro ao executar a tarefa de preço:", error);
-    //     res.status(500).send("Erro interno do servidor");
-    // }
+    } catch (error) {
+        res.status(500).send("Erro ao executar a tarefa de preço:" +error);
+    }
 });
 
 module.exports = router;
