7bc05b6b27c939a1134e6b01b5cd343a486ea0fa
alberto.lopes <cast.aberto@pe.sebrae.com.br>
2025-06-15 16:35:53 -0300

    Noticia schema

---
+++ src/db/Noticia.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const noticiaSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    resumo: {
+        type: String,
+        required: true
+    },
+    textoCompleto: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        required: true
+    },
+    linkOrigem: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Noticia = mongoose.model(
+    'Noticia',
+    noticiaSchema,
+    process.env.NODE_ENV
+);
+
+module.exports = Noticia;
